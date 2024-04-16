c139a563d6e9e739de1f048f323b8d2c2afe2ea5
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-04-17 04:49:15 +0700

    [RED] Define ProductRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartbeproduct/repository/ProductRepository.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.heymartbeproduct.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import id.ac.ui.cs.advprog.heymartbeproduct.model.Product;
+
+public class ProductRepository {
+
+    public Optional<Product> findProductById(String id) {
+        // Implementations will be added here
+        return null;
+    }
+
+    public Product saveProduct(Product product) {
+        // Implementations will be added here
+        return null;
+    }
+
+    public void deleteProductById(String id) {
+        // Implementations will be added here
+    }
+
+    public List<Product> getAllProducts() {
+        // Implementations will be added here
+        return null;
+    }
+}
