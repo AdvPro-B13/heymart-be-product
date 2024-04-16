899adae21f4d381e891ced93f1ed0af76b4eb82d
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-04-17 04:50:17 +0700

    [GREEN] Add implementations for ProductRepository

--- src/main/java/id/ac/ui/cs/advprog/heymartbeproduct/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartbeproduct/repository/ProductRepository.java
@@ -1,28 +1,41 @@
 package id.ac.ui.cs.advprog.heymartbeproduct.repository;
 
+import id.ac.ui.cs.advprog.heymartbeproduct.model.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-import id.ac.ui.cs.advprog.heymartbeproduct.model.Product;
-
 public class ProductRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    @Transactional
     public Optional<Product> findProductById(String id) {
-        // Implementations will be added here
-        return null;
+        Product product = entityManager.find(Product.class, id);
+        return Optional.ofNullable(product);
     }
 
+    @Transactional
     public Product saveProduct(Product product) {
-        // Implementations will be added here
-        return null;
+        if (product.getId() == null || findProductById(product.getId()).isEmpty()) {
+            entityManager.persist(product);
+        } else {
+            product = entityManager.merge(product);
+        }
+        return product;
     }
 
+    @Transactional
     public void deleteProductById(String id) {
-        // Implementations will be added here
+        Product product = findProductById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        entityManager.remove(product);
     }
 
+    @Transactional
     public List<Product> getAllProducts() {
-        // Implementations will be added here
-        return null;
+        return entityManager.createQuery("SELECT p FROM Product p", Product.class).getResultList();
     }
 }
